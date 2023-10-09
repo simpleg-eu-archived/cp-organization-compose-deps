227349f6d15a4000cb75f59a7cbef00a3622e485
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-10-09 08:55:14 +0200

    + separate docker compose from db initialization

---
+++ db_init.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    curl https://downloads.mongodb.com/compass/mongosh-2.0.1-linux-x64.tgz --output mongosh.tgz
+    tar -zxvf mongosh.tgz
+
+    mv mongosh-2.0.1-linux-x64/bin/* ./
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    curl https://downloads.mongodb.com/compass/mongosh-2.0.1-darwin-arm64.zip --output mongosh.zip
+    unzip -o mongosh.zip
+
+    mv mongosh-2.0.1-darwin-arm64/bin/* ./
+fi
+
+chmod +x mongosh
+./mongosh $CP_ORGANIZATION_MONGODB_CONNECTION_URI --username $CP_ORGANIZATION_MONGODB_USERNAME --password $CP_ORGANIZATION_MONGODB_PASSWORD --file organization_permission.js --file organization.js --file role.js --file member.js --file invitation_code.js
